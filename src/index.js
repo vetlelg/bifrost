a51bf1da5e0f55fd3cb7ad75b6b4f17d1b4b24ae
vetlelg <61113128+vetlelg@users.noreply.github.com>
2023-10-20 18:55:58 +0200

    Test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React,{ useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import { faHome, faUser, faCog } from '@fortawesome/free-solid-svg-icons'
 import { Nav } from '@intility/bifrost-react'
